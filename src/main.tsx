8ee0bc741a85e054bdc653c7dc2b7977f5a5c8fb
saadfrhan <saadfarhan347@gmail.com>
2024-08-12 12:43:38 -0700

    change default theme to system

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-import { ThemeProvider } from './components/theme-provider.tsx';
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
-import '@fontsource-variable/roboto-flex';
+import "@fontsource-variable/roboto-flex";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-    <App />
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
-)
+);
